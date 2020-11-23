3686cfff8fe54b156e4452cd6b37846959211b9c
Viktor1Likashev <lukashev-viktor2012@yandex.ru>
2020-11-23 16:07:09 +0300

    commit

---
+++ Logger/Logger.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+//для упрощения я для записи передаю тип сообщения и само сообщение в конструкторе Logger. 
+namespace Logger
+{
+    class Logger
+    {
+        private string path = "logger.txt";//куда будут записываться данные
+        private string type;//тип сообщения
+        string rx; // переменная куда будет считано регулярное выражение из файла
+  
+        private string GetSettings()
+        {
+          
+            try
+            {
+                using (FileStream file = new FileStream("my.txt",FileMode.Open)) //открываем файл с шаблоном выражения
+                {
+                    using (StreamReader rider = new StreamReader(file, Encoding.UTF8))
+                    {
+                        string settings = rider.ReadToEnd();//считываем данные в переменную и возвращаем ее вызывающему методу
+                        return settings;
+                           
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return "none";
+            }
+        }
+        public void record(string TextExeption,string TypeExeption)
+        {
+            rx = GetSettings();//получаем регулярное выражение
+
+            string streaming = Convert.ToString(DateTime.Now) + " " + (string)Environment.UserName + " "  + TypeExeption + " " + TextExeption;//получаем строку которую нужно записать в файл
+            Regex reg = new Regex(rx);
+            if (reg.IsMatch(streaming)) //сравниваем записываемую строку с шаблоном регулярного выражения
+            {
+                try
+                {
+                    //записываем данные в поток
+                    Console.WriteLine("попытка записи данных в файл...");
+                    using (FileStream file = new FileStream(path, FileMode.Append))
+                    {
+
+                        using (StreamWriter wr = new StreamWriter(file, Encoding.UTF8))
+                        {
+                            wr.WriteLine(streaming);
+                            
+                        }
+
+                    }
+                    Console.WriteLine("данные успешно записаны и сохранены!");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                //в случае несоответствия записываемой строки и регулярного выражения выводим соответствующее сообщение
+                Console.WriteLine("Incorrected data for Regex");
+                Console.WriteLine(streaming);
+                Console.WriteLine(rx);
+
+            }
+        }
+    }
+}
---
+++ Logger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //в случае запуска этой программы на другом пк, она сначала создаст файл, запишет туда регулярное выражение а потом будет его же и считывать
+            using (FileStream str = new FileStream("my.txt", FileMode.Create))
+            {
+                using (StreamWriter sw = new StreamWriter(str, Encoding.UTF8))
+                {
+                    sw.Write(@"^\d{2}\.\d{2}\.\d{4}\s\d*\:\d{2}\:\d{2}\s\S*\s\w*\w*");
+                }
+            }
+                Logger lg = new Logger();
+            lg.record("ошибка отказано в доступе к ресурсу","Error");
+            lg.record("попытка несанкционированного доступа к ресурсу", "information");
+        }
+    }
+}
